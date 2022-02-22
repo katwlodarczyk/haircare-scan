9972e6a1acbdf5a882fa0e1083ce03c394a2424d
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-02-22 12:20:59 +0000

    Added tailwind, started working on tab bar

---
+++ src/components/TabBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav
+    class="z-30 fixed bottom-0 w-full bg-brand-teal text-white shadow-xl flex flex-row items-center justify-between px-7 pt-4 pb-8 text-xs"
+  >
+    <RouterLink to="/" class="flex flex-col justify-center items-center space-y-2.5">Home</RouterLink>
+    <RouterLink to="/about" class="flex flex-col justify-center items-center space-y-2.5">About</RouterLink>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+  setup() {},
+};
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-purple": "#9764C7",
+        "brand-pink": "#F2AEE7",
+        "brand-nude": "#F8F5F7",
+      },
+      fontSize: {
+        xxxs: "10px",
+      },
+      fontFamily: {
+        koho: "KoHo",
+      },
+    },
   },
   plugins: [],
-}
+};
