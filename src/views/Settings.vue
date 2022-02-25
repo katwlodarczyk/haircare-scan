c1b72b748cdc1e7a28ecf99f8a5209c180ea42bb
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-02-25 11:38:50 +0000

    ability to change email and password FE

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,9 +7,21 @@
       >
         <div class="flex flex-row space-x-4">
           <span>Email:</span>
-          <div class="font-medium">jane@example.com</div>
+          <div class="font-medium">
+            <div v-if="openEmailEdit === true">
+              <input
+                type="text"
+                name="email"
+                placeholder="jane@example.com"
+                class="rounded-lg bg-brand-pale px-2 border border-brand-pink outline-none ring-1 ring-brand-pink focus:shadow-brand-pink"
+              />
+            </div>
+            <span v-else class="">jane@example.com</span>
+          </div>
         </div>
         <svg
+          @click="openEmailEdit = true"
+          v-if="openEmailEdit === false"
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 self-end text-brand-purple"
           fill="none"
@@ -23,6 +35,20 @@
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
           />
         </svg>
+        <svg
+          @click="saveEmailEdit"
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 self-end text-brand-pink"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+            clip-rule="evenodd"
+          />
+        </svg>
       </div>
 
       <div
@@ -30,9 +56,21 @@
       >
         <div class="flex flex-row space-x-4">
           <span>Password:</span>
-          <div class="font-medium">******</div>
+          <div class="font-medium">
+            <div v-if="openPasswordEdit === true">
+              <input
+                type="password"
+                name="password"
+                placeholder="******"
+                class="rounded-lg bg-brand-pale px-2 border border-brand-pink outline-none ring-1 ring-brand-pink focus:shadow-brand-pink"
+              />
+            </div>
+            <span v-else class="">******</span>
+          </div>
         </div>
         <svg
+          @click="openPasswordEdit = true"
+          v-if="openPasswordEdit === false"
           xmlns="http://www.w3.org/2000/svg"
           class="h-6 w-6 self-end text-brand-purple"
           fill="none"
@@ -44,6 +82,20 @@
             stroke-linejoin="round"
             stroke-width="2"
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+        <svg
+          @click="savePasswordEdit"
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 self-end text-brand-pink"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+            clip-rule="evenodd"
           />
         </svg>
       </div>
@@ -98,11 +150,27 @@
 </template>
 
 <script>
+import { ref } from "vue";
 import ViewHeader from "../components/ViewHeader.vue";
 export default {
   components: {
     ViewHeader,
   },
-  setup() {},
+  setup() {
+    const openEmailEdit = ref(false);
+    const openPasswordEdit = ref(false);
+
+    const saveEmailEdit = () => {
+      console.log("saved email edit");
+      openEmailEdit.value = false;
+    };
+
+    const savePasswordEdit = () => {
+      console.log("saved password edit");
+      openPasswordEdit.value = false;
+    };
+
+    return { openEmailEdit, saveEmailEdit, openPasswordEdit, savePasswordEdit };
+  },
 };
 </script>
