0e9cff67049a1e1e8a0229d970b543e3f2fecf16
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-03-04 14:26:49 +0000

    added link to scan list item

--- src/components/ScanListItem.vue
+++ src/components/ScanListItem.vue
@@ -2,11 +2,20 @@
   <div
     class="flex flex-row justify-between odd:bg-brand-pale even:bg-brand-nude px-6 py-5 space-x-4"
   >
-    <img v-if="withPhoto" src="" alt="" />
-    <img v-else src="../assets/icons/eye.svg" alt="eye icon" />
+    <div
+      @click="
+        $router.push({
+          name: 'analyzed',
+          params: { id: 1, scan: capturedImage },
+        })
+      "
+      class="flex flex-row space-x-4"
+    >
+      <img v-if="withPhoto" src="" alt="" />
+      <img v-else src="../assets/icons/eye.svg" alt="eye icon" />
 
-    <h2 class="truncate">{{ title }}</h2>
-
+      <h2 class="truncate">{{ title }}</h2>
+    </div>
     <div class="relative">
       <svg
         @click="displayMenu = !displayMenu"
@@ -23,25 +32,26 @@
           d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
         />
       </svg>
-      <div v-if="displayMenu"
+      <div
+        v-if="displayMenu"
         class="z-40 origin-top-right absolute mt-1 right-0 w-40 rounded-md shadow-md bg-brand-pale focus:outline-none"
       >
         <div class="py-1 w-full float-right">
           <div
-            onClick="{addIngredientsToShoppingList}"
+            @click="addToFavourite"
             class="text-gray-700 w-full justify-end flex flex-row space-x-2 items-center px-4 py-3 text-xs"
             id="0"
           >
             <span>Add to favourites</span>
-            <img class="w-5 h-5" src="../assets/icons/favourite-inactive.svg" >
+            <img class="w-5 h-5" src="../assets/icons/favourite-inactive.svg" />
           </div>
           <div
-            onClick="{addIngredientsToShoppingList}"
+            @click="removeScan"
             class="text-gray-700 w-full justify-end flex flex-row space-x-2 items-center px-4 py-3 text-xs"
-            id="0"
+            id="1"
           >
             <span>Remove a scan</span>
-            <img class="w-5 h-5" src="../assets/icons/delete.svg" >
+            <img class="w-5 h-5" src="../assets/icons/delete.svg" />
           </div>
         </div>
       </div>
@@ -51,6 +61,9 @@
 
 <script>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useToast } from "vue-toastification";
+
 export default {
   props: {
     title: {
@@ -63,8 +76,29 @@ export default {
     },
   },
   setup() {
+    const router = useRouter();
     let displayMenu = ref(false);
-    return { displayMenu };
+    const toast = useToast();
+
+    const addToFavourite = () => {
+      console.log("Added to favourite");
+      toast.info("Added to favourites!", {
+        position: "bottom-right",
+        timeout: 1000,
+        closeOnClick: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.1,
+        showCloseButtonOnHover: false,
+        hideProgressBar: true,
+        closeButton: false,
+        icon: false,
+        rtl: false,
+      });
+    };
+
+    return { router, displayMenu, addToFavourite, toast };
   },
 };
 </script>
